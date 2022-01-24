b8f50859b69f100b3d471b1eef1bdafcd39f313c
dbwls9675 <dbwls9675@nate.com>
2022-01-24 17:25:37 +0900

    URL객체, jSoup사용, JSON라이브러리를 이용한 JFrame에 이미지 목록 만들기

---
+++ src/ExUrlResource01/Ex02Url.java
@@ -0,0 +1,49 @@
+package ExUrlResource01;
+//이미지 불러오기
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Ex02Url {
+
+	public Ex02Url() throws IOException {
+		String imgUrlPath = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA3MjJfMTI3%2FMDAxNjI2OTEyMzEwNDE2.xhG1D3Py9QL36ZiLWZOQ4wRHnPl2SpZclSyQjFbLL2Mg.cRUeLXswuSkUgVWUmLh1k4woSL6HWcw7su97AbR7MyUg.JPEG.sauooo%2F20210619%25A3%25DF195020.jpg&type=sc960_832";
+		URL url = new URL(imgUrlPath);
+
+		InputStream is = url.openStream();
+		DataInputStream dis = new DataInputStream(is);
+
+		File dir = new File("./images");
+		if(!dir.exists()) {
+			if(dir.mkdir()) {
+				System.out.println("새 디렉토리 생성 완료!");
+			}
+		}
+		
+		String saveFilePath = "photo.jpg";
+		File file = new File(dir, saveFilePath);
+		FileOutputStream fos = new FileOutputStream(file);
+		DataOutputStream dos = new DataOutputStream(fos);
+		int data = 0;
+		while ((data = dis.read()) != -1) {
+			// 파일로 저장 하기
+			dos.write(data);
+		}
+
+		if (dis != null)
+			dis.close();
+		if (is != null)
+			is.close();
+		if (dos != null)
+			dos.close();
+		if (fos != null)
+			fos.close();
+	}
+	public static void main(String[] args) throws IOException {
+		new Ex02Url();
+	}
+}
---
+++ src/ExUrlResource02/Ex03JSoup.java
@@ -0,0 +1,30 @@
+package ExUrlResource02;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+//JSoup 다운 → eclipse-workspace 넣기 → BuildPath → 라이브러리에 Add External.jar에서 추가 
+public class Ex03JSoup {
+	
+	public Ex03JSoup() throws IOException {
+		String urlPath ="https://www.w3schools.com/css/css_selectors.asp";
+		Document doc = Jsoup.connect(urlPath).get();
+		System.out.println(doc.title());//창 타이틀 가져오기
+		
+		
+		Elements lis = doc.select("#main ul li");
+		for(Element el : lis) {
+			System.out.printf("%s, %s\n",el.select("a[href]").attr(""),el.text());
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Ex03JSoup();
+		
+	}
+
+}
